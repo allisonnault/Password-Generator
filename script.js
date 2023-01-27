3a78df9caa993d7202832d470d17a1f7a7c49b0c
allisonnault <allisonrnault@gmail.com>
2023-01-26 21:51:53 -0600

    added character selection

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'];
+
+var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var specialChar = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\ ", "\]", "^", "_", "`", "{", "|", "}", "~"];
+
+
 // Write password to the #password input
 function writePassword() {
     var passwordLenght = parseInt(prompt("How many characters would you like your password to contain?"));
-    console.log(passwordLenght);}
+    console.log(passwordLenght);
 
-if ((passwordLenght < 8) || (passwordLenght > 129)) {
-  alert("Please select a number between 8 and 129");}
-//    var hasNumeric = confirm("Click OK to confirm including numeric characters.");
-//       console.log(hasNumeric);}
-//     else {  
-//       alert("Please select a number between 8 and 129");}
+if((passwordLenght < 8) || (passwordLenght > 129)) {
+  alert("Please select a number between 8 and 129");
+  return writePassword();}
+
+var hasNumeric = confirm("Click OK to confirm including numeric characters.");
+console.log(hasNumeric);
+
+var hasLower = confirm("Click OK to confirm including Lower characters.");
+console.log(hasLower);
   
-//       var hasLower = confirm("Click OK to confirm including Lower characters.");
-//       console.log(hasLower);
+var hasUpper = confirm("Click OK to confirm including Upper characters.");
+console.log(hasUpper);
   
-//       var hasUpper = confirm("Click OK to confirm including Upper characters.");
-//       console.log(hasUpper);
-  
-//       var hasSpecial = confirm("Click OK to confirm including Special characters.");console.log(hasSpecial);
-  
-      
-        // if ((hasNumeric === false) && (hasLower === false) && (hasUpper === false) && (hasSpecial === false)){
-        //     alert('Must select at least one character type');
-  
+var hasSpecial = confirm("Click OK to confirm including Special characters.");
+console.log(hasSpecial);
+
+if ((hasNumeric === false) && (hasLower === false) && (hasUpper === false) && (hasSpecial === false)){
+  alert('Must select at least one character type');
+  return writePassword();}
+
+
     
 
 
 
-
+}
       // alert("Please select a number between 8 and 129");
     
   
@@ -52,4 +63,8 @@ generateBtn.addEventListener("click", writePassword);
 
 // var specialChar = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\ ", "\]", "^", "_", "`", "{", "|", "}", "~"];
 
-// var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z']
+
+// var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
